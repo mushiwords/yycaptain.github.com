dfd7363beb840b2e28219c3121efe213fd596d03
yaung <gaoyang615@163.com>
2014-08-06 18:51:26 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8">    
+<meta http-equiv="X-UA-Compatible" content="chrome=1">   
+<title>Yaung.github.com by yaung</title>
+</head>
+<body>
 
 <?php
 
  $time=time();
  echo $time;
 ?>
+</body>
+</html>
